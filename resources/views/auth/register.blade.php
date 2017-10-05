2425969d91ab55201bae9eccccfe4e74c86ae973
erikokos <stegvy@ya.ru>
2017-10-05 16:02:57 +0300

    05.10

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Registry Page</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href=/css/bootstrap.css">
+    <!-- Custom styles for this template -->
+    <link href="/css/cover.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">RegDem</h3>
+              <nav class="nav nav-masthead">
+                <a class="nav-link active" href="#">Main</a>
+                <a class="nav-link" href="#">Login</a>
+                <a class="nav-link" href="#">Registry</a>
+              </nav>
+            </div>
+          </div>
+          <div class="container">
+
+            <form class="form-signin">
+              <h2 class="form-signin-heading">Please sign in</h2>
+              <label for="inputEmail" class="sr-only">Email address</label>
+              <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+              <label for="inputPassword" class="sr-only">Password</label>
+              <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="remember-me"> Remember me
+                </label>
+              </div>
+              <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+            </form>
+
+          </div> <!-- /container -->
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
