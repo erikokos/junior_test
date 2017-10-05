e410158ddfa3662af5ea572c2abd2782251bcb23
erikokos <stegvy@ya.ru>
2017-10-05 16:17:23 +0300

    05.10-(2)

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Welcome Page</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href=/css/bootstrap.css">
+    <!-- Custom styles for this template -->
+    <link href="/css/cover.css" rel="stylesheet">
+</head>
+<body>
+<div class="site-wrapper">
+    <div class="site-wrapper-inner">
+        <div class="cover-container">
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <h3 class="masthead-brand">RegDem</h3>
+                    <nav class="nav nav-masthead">
+                        <a class="nav-link active" href="#">Main</a>
+                        <a class="nav-link" href="#">Login</a>
+                        <a class="nav-link" href="#">Registry</a>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="inner cover">
+                <h1 class="cover-heading">YOU SHALL NOT PASS!<br/><i class="ps">...login first</i></h1>
+                    <a href="#" class="btn btn-lg btn-secondary">Do it now</a>
+                </p>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Bootstrap core JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>
